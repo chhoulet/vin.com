41076b5bbc729e5768ac1e570ff5ff5918b14287
Christophe Houlet <chhoulet@yahoo.fr>
2015-03-10 11:45:47 +0100

    Creation de la fonction creation de commentaire avec persist en BDD + tests

---
+++ src/Vin/FrontOfficeBundle/Controller/MessageController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Vin\FrontOfficeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Vin\FrontOfficeBundle\Entity\Message;
+
+use Vin\FrontOfficeBundle\Form\MessageType;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class MessageController extends Controller
+{
+    public function createmessageAction(Request $request)
+    {
+        $em = $this -> getDoctrine() -> getManager();
+        $message = new Message();
+        $form = $this -> createForm(new MessageType(),$message);
+
+        $form -> handleRequest($request);
+
+        if($form -> isValid()){
+            $message -> setDateMessage( new \DateTime('now'));
+            $em -> persist($message);
+            $em -> flush();
+
+            return $this -> redirect($this-> generateUrl('vin_front_office_bundle_message'));
+        }
+
+        return $this -> render('VinFrontOfficeBundle:Message:message.html.twig',array('form'=>$form->createView()));
+    }
+}
---
+++ src/Vin/FrontOfficeBundle/Form/MessageType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Vin\FrontOfficeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MessageType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('content')
+            ->add('emailMessage')
+            ->add('valider','submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Vin\FrontOfficeBundle\Entity\Message'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'vin_frontofficebundle_message';
+    }
+}
